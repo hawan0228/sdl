4af8c8d65ad288f6d1e9950bb1c0247d79cbd25c
haoan <an0610102@gmail.com>
2024-12-26 03:27:43 +0800

    push

--- game/game.cpp
+++ game/game.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include "player.h"
 
-const int SCREEN_WIDTH = 800;
+const int SCREEN_WIDTH = 700;
 const int SCREEN_HEIGHT = 600;
 
 bool init(SDL_Window*& window, SDL_Renderer*& renderer) {
